3b5229fd6b17d60d3bc0f8f2d03f40ac452eca6d
MIAGEJordan <jordan_vn@hotmail.com>
2015-05-11 14:29:41 +0200

    Adrien <3

--- routes/views/group.js
+++ routes/views/group.js
@@ -4,14 +4,7 @@ var keystone = require('keystone'),
 exports = module.exports = function(req, res) {
   var view = new keystone.View(req, res),
 		locals = res.locals;
-
-	// Init locals
-//	locals.section = 'blog';
-//	locals.page.title = 'Blog - SydJS';
-//	locals.filters = {
-//		category: req.params.category
-//	};
-
+		
 	locals.data = {
 		groups: [],
 		posts: []
