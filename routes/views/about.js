5a93441545ea6d0a50294d3e5da382170863dde8
aureliench <aurelien.chassereau@gmail.com>
2015-05-26 14:44:57 +0200

    Bug Perso

--- routes/views/about.js
+++ routes/views/about.js
@@ -9,11 +9,11 @@ exports = module.exports = function(req, res) {
 	locals.page.title = 'About SydJS';
 
 	locals.organisers = [
-		{ name: 'Craig Sharkie', image: '/images/organiser-craig_sharkie.jpg', twitter: 'twalve',       title: 'Founder, MC, coordinator' },
+		{ name: 'Aurélien Chassereau', image: '/images/organiser-craig_sharkie.jpg', twitter: 'twalve',       title: 'Milieu de terrain' },
 		// { name: 'Thinkmill', image: '/images/organiser-thinkmill.jpg',     twitter: 'thethinkmill', title: 'Site coordinator' },
-		{ name: 'Gil Davidson',     image: '/images/organiser-gil_davidson.jpg',     twitter: 'iamnotyourbroom',   title: 'Atlassian coordinator' },
-		{ name: 'Adam Ahmed',    image: '/images/organiser-adam_ahmed.jpg',    twitter: 'hitsthings',   title: 'Atlassian coordinator' },
-		{ name: 'Lachlan Hardy', image: '/images/organiser-lachlan_hardy.jpg', twitter: 'lachlanhardy', title: 'Community coordinator' }
+		{ name: 'Matthieu Peyres',     image: '/images/organiser-gil_davidson.jpg',     twitter: 'iamnotyourbroom',   title: 'Gardien de but' },
+		{ name: 'Adrien Valin',    image: '/images/organiser-adam_ahmed.jpg',    twitter: 'hitsthings',   title: 'Pistolero' },
+		{ name: 'Jordan Vo Ngoc', image: '/images/organiser-lachlan_hardy.jpg', twitter: 'lachlanhardy', title: 'Numéro 10 - Organisateur' }
 	]
 
 	view.render('site/about');
