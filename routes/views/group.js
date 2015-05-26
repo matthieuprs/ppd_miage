07cc461b6cb606f6f31d3611bea09a825991ef06
MIAGEJordan <jordan_vn@hotmail.com>
2015-05-26 13:53:39 +0200

    group

--- routes/views/group.js
+++ routes/views/group.js
@@ -4,32 +4,35 @@ var keystone = require('keystone'),
 exports = module.exports = function(req, res) {
   var view = new keystone.View(req, res),
 		locals = res.locals;
-		
+
 	locals.data = {
 		groups: [],
 		posts: []
 	};
 
   view.on('init', function(next) {
-
+		//Rechercher les groupes de la personne connectée.
     keystone.list('Organisation').model.find().where('author',res.locals.user.id).populate('author').exec(function(err, orgas) {
-
-      console.log(res.locals.user.id);
-
-      if (err) {
-        return next(err);
-      }
-
+			// la liste des groupes de la personne dont elle est créatrice, donc admin
       locals.orgas = orgas;
-      console.log(locals.orgas.author);
-
-      console.log(locals.orgas);
       next();
     });
 
+		//récupère la liste de mes groupes suivis
+		keystone.list('User').model.findById(res.locals.user.id).populate('groupes').exec(function(err,user){
+			locals.test = user.groupes;
+			console.log(locals.test);
+		});
+
+		//récupère la liste des membres du groupes selectionnés
+
     keystone.list('Organisation').model.findOne().populate('author').exec(function(err, post) {
-      console.log(post.author.name);
+    //récupère la liste des autheurs des groupes
     });
+		//récupère la liste des posts d'un groupe
+		//keystone.liste('Post').model.find().where('groupe').in([])
+
+
 
   });
 
