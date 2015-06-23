10dc5f0429139d638c0ae5162dfd3ca1374ab291
aureliench <aurelien.chassereau@gmail.com>
2015-06-23 15:17:25 +0200

    About us

--- routes/views/about.js
+++ routes/views/about.js
@@ -9,11 +9,11 @@ exports = module.exports = function(req, res) {
 	locals.page.title = 'About Us';
 
 	locals.organisers = [
-		{ name: 'Aurélien Chassereau', image: '/images/organiser-craig_sharkie.jpg', twitter: 'twalve',       title: 'Milieu de terrain' },
+		{ name: 'Aurélien Chassereau', image: '/images/organiser-craig_sharkie.jpg', twitter: 'Aurelien_Ch',       title: 'Milieu de terrain' },
 		// { name: 'Thinkmill', image: '/images/organiser-thinkmill.jpg',     twitter: 'thethinkmill', title: 'Site coordinator' },
-		{ name: 'Matthieu Peyres',     image: '/images/organiser-gil_davidson.jpg',     twitter: 'iamnotyourbroom',   title: 'Gardien de but' },
-		{ name: 'Adrien Valin',    image: '/images/organiser-adam_ahmed.jpg',    twitter: 'hitsthings',   title: 'Pistolero' },
-		{ name: 'Jordan Vo Ngoc', image: '/images/organiser-lachlan_hardy.jpg', twitter: 'lachlanhardy', title: 'Numéro 10 - Organisateur' }
+		{ name: 'Matthieu Peyres',     image: '/images/organiser-gil_davidson.jpg',     twitter: 'SuperDragz',   title: 'Gardien de but' },
+		{ name: 'Adrien Valin',    image: '/images/organiser-adam_ahmed.jpg',    twitter: 'AdrienValin',   title: 'Pistolero' },
+		{ name: 'Jordan Vo Ngoc', image: '/images/organiser-lachlan_hardy.jpg', facebook: 'jordanquang.vongoc', title: 'Numéro 10 - Organisateur' }
 	]
 
 	view.render('site/about');
